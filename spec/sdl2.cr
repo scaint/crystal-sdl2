aa9c9cb3cca2c661a0c8f6f9d56e9b093915af5c
Eugene Sheverdin <me@scaint.name>
2015-10-30 00:18:21 +0200

    Test build

--- spec/sdl2.cr
+++ spec/sdl2.cr
@@ -6,5 +6,9 @@ describe "SDL2" do
     it "initializes SDL" do
       SDL2.init.should be_nil
     end
+
+    it "initializes only specified subsystems" do
+      SDL2.init(SDL2::INIT::EVENTS | SDL2::INIT::TIMER).should be_nil
+    end
   end
 end
